0e1b2d5ed68504b1c784b099e94b56d60a4f2005
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-03-16 14:03:55 -0300

    feat: added user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,66 @@
+import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
+
+export const listUsersController = async (
+  request: Request,
+  response: Response,
+) => {
+  // const users = await prisma.user.findMany({select: {id: true,},});
+  const users = await prisma.user.findMany();
+
+  response.send({
+    users,
+  });
+};
+
+export const findOneUserController = async (
+  request: Request,
+  response: Response,
+) => {
+  const { userId } = request.params;
+
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+
+  if (!user) {
+    return response.status(404).send({ error: "User not found" });
+  }
+
+  response.send({
+    user,
+  });
+};
+
+export const createUserController = async (
+  request: Request,
+  response: Response,
+) => {
+  const { name, email } = request.body;
+
+  if (!name && !email) {
+    return response.send({
+      error: "A valid name and email is required",
+    });
+  }
+
+  const userEmailAlready = await prisma.user.findUnique({
+    where: { email },
+    select: {
+      id: true,
+    },
+  });
+
+  if (userEmailAlready) {
+    return response.status(400).send({ error: "Already registered user" });
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  response.send({
+    user,
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import {
+  createUserController,
+  findOneUserController,
+  listUsersController,
+} from "./controllers/user.controller";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Welcome to Express API");
 });
+
+app.get("/users", listUsersController);
+
+app.get("/users/:userId", findOneUserController);
+
+app.post("/users", createUserController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
